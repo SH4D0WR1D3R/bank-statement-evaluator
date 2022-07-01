37b551d205ae40d798a9478f27a6435f5206a2f7
SH4D0WR1D3R <53613777+SH4D0WR1D3R@users.noreply.github.com>
2022-07-01 23:39:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# Bank Statement Evaluator
+# Input: bank statement for the month
+# Output: excel spreadsheet with information grouped
 
+# Issues: Understanding what different names mean
+# Can easily map the direct meanings (name, value, date)
+# Not so much interpreting it to group by rent, social etc
+
+
+# interpret pdf
+    # 3 different formats
+        # santander, lloyds, monzo
+        # need to be able to know where to look
+# write to excel spreadsheet
